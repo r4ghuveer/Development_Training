853c4fd7e0aa4af50f2249f78150e41eaf616af0
r4ghuveer <raghuveerofficial08@gmail.com>
2023-01-22 07:37:52 +0530

    Using express & postman to check request and response in app.js

--- Backend_Node_js/1st_Express/app.js
+++ Backend_Node_js/1st_Express/app.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = 80;
 app.get('/',(req,res) =>{
-    res.send("This is my first express app");
+    res.send("Home page");
 
+});
+app.get('/about',(req,res) =>{ //for get request  
+    res.send("About page");    //sends response to get request
+
+});
+
+app.post('/',(req,res)=>{
+    res.send("post !!");
+});
+//for including status of the request and response we include status
+app.get('/help',(req,res)=>{        //since there is no help page
+    res.status(404).send("Maro mujhe");  
 });
 
 app.listen(port,()=>{

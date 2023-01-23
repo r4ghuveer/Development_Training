afc4c98474a7426338911888112a29a99a47c551
r4ghuveer <raghuveerofficial08@gmail.com>
2023-01-23 09:59:15 +0530

    Created a web page form (only front-end)

--- Backend_Node_js/1st_Express/app.js
+++ Backend_Node_js/1st_Express/app.js
@@ -1,6 +1,31 @@
 const express = require('express');
+const path= require('path');
 const app = express();
 const port = 80;
+//for serving static files EXPRESS SPECEFIC STUFF
+app.use('/static',express.static('static')) //now we can use /static url to access code in static folder.
+
+// Set the template engine as pug PUG SPECIFIC STUFF
+app.set('view engine','pug')
+app.set('views',path.join(__dirname,'views'))// Set the views directory
+
+
+//END POINTS
+//our pug demo end point
+app.get('/demo',(req,res) =>{ 
+    res.status(200).render('demo',{title:'This is title',message:'This is the message, How are you doing sir!'});   
+
+});
+app.get('/',(req,res) =>{ 
+    res.status(200).render('index.pug',{title:'This is title',message:'This is the message, How are you doing sir!'});   
+
+});
+//creating object to send to index.pug
+const obj = {'h1':'PUG XP','heading':'PUG using an plain HTML','content':'Hello guys my name is r4ghuveer and this is a paragraph......'}
+app.get('/index',(req,res)=>{
+    res.status(200).render('index.pug',obj);
+})
+
 app.get('/',(req,res) =>{
     res.send("Home page");
 
@@ -10,9 +35,7 @@ app.get('/about',(req,res) =>{ //for get request
 
 });
 
-app.post('/',(req,res)=>{
-    res.send("post !!");
-});
+
 //for including status of the request and response we include status
 app.get('/help',(req,res)=>{        //since there is no help page
     res.status(404).send("Maro mujhe");  

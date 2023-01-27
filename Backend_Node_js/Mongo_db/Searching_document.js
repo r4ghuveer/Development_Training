a5f01f3effa445ddb636430acb41373606b85ea8
r4ghuveer <raghuveerofficial08@gmail.com>
2023-01-27 05:44:30 +0530

    AND & OR operator while filtering data

--- Backend_Node_js/Mongo_db/Searching_document.js
+++ Backend_Node_js/Mongo_db/Searching_document.js
@@ -6,4 +6,22 @@
 //inserting data or document in database using mongodb
 //use 1stDB
 
-db.item.find({age: 21}); //This querry will give all the objects that has age = 21
+db.item.find({age: 21}); //This querry will return all the objects that has age = 21
+
+
+//gt : - greater than 
+//lt : - lesser than.
+
+
+db.item.find({age :{$gte: 20}})// Returns all objects that has age >=20
+
+db.item.find({age :{$gt: 20}})// Returns all objects that has age >20 ##not equal to 20.  "e" is removed.
+
+db.item.find({age :{$lt: 20}})// Returns all objects that has age less than <20 #&#not equal to 20.  "e" is removed.
+
+//AND OPERATOR : - i.e. Age should be greater than 20 and gender should be male both should satisfy.
+//can use multiple filters
+db.item.find({age :{$gt: 20}, gender:'male'})
+
+//OR OPERATOR : - if satify any of the filter than true;
+db.item.find($or[{age :{$gt: 20}, gender:'male'}])

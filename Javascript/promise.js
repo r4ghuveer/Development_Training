6b0588ca793233f94f2d5ff0bea24ee367c295ac
r4ghuveer <raghuveerofficial08@gmail.com>
2023-01-29 07:59:04 +0530

    first json fetch

--- Javascript/promise.js
+++ Javascript/promise.js
@@ -2,7 +2,7 @@ let p1 = new Promise((resolve, reject) => {
     console.log("Promise is pending")
     setTimeout(() => {
             // console.log("I am a promise and I am resolved")
-            resolve(true)
+            resolve(true) // this "true" or any other value here will be send to value in p1.then()
     }, 5000)
 })
 
@@ -10,7 +10,7 @@ let p2 = new Promise((resolve, reject) => {
     console.log("Promise is pending")
     setTimeout(() => {
             // console.log("I am a promise and I am rejected")
-            reject(new Error("I am an error"))
+            reject(new Error("I am an error"))// this "error" or any other value here will be send to value in p2.catch or p2.then(value),(error));
     }, 5000)
 })
 

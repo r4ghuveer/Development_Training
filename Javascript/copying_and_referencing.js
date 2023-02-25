746cf1570e1d13c4eca112e2db2fe13c9669497a
r4ghuveer <raghuveerofficial08@gmail.com>
2023-02-25 22:56:19 +0530

    copying and referencing

--- Javascript/copying_and_referencing.js
+++ Javascript/copying_and_referencing.js
@@ -5,3 +5,18 @@ const names = ["ram","sam"];
 
 names.push("dam");// but this will work since we are just changing what the constant variable is pointing to, not the variable itself. 
 console.log(names);
+
+
+//For copying a object or a array
+
+const obj={
+    name:"raghuveer",
+    age: 20,
+    sex: "male"
+}
+
+const new_obj= {...obj}; // here we use the spread operator(...) to pull out the elements inside obj to new_obj
+
+const new_obj2={...obj}; // but if we don't do that it will create
+new_obj2.gender="male";
+console.log(obj);

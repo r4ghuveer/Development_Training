b119d51e54d8be17496dba8429dd47d27771c166
r4ghuveer <raghuveerofficial08@gmail.com>
2023-03-04 23:16:12 +0530

    serving

--- Backend_Node_js/Serving.js
+++ Backend_Node_js/Serving.js
@@ -4,7 +4,6 @@ const filecontent = fs.readFileSync('G:\\Web_dev\\Backend_Node_js\\Events.html')
 const server = http.createServer((req,res)=>{
     const url=req.url;
     if(url==='/'){
-        
         res.write('<html>')
         res.write('<body>Hello everyone!<form method="POST" action="/message"><input type="text" name="message"><button type="submit">Click!</button></form></body>')//Response body; Check these on network tab of browers
         res.write('</html>')

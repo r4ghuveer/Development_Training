3f55dd2fdbebe796d980a931d8b88dcbe68b4adf
r4ghuveer <raghuveerofficial08@gmail.com>
2023-03-04 23:17:55 +0530

    -

--- Javascript/Assignment_udemy.js
+++ Javascript/Assignment_udemy.js
@@ -10,7 +10,6 @@ const server = http.createServer((req,res)=>{
         res.write('<html><body><li><ul>Raghuveer</ul><ul>Awesome</ul></li></body></html>');
         res.end();
     }
-    
     if(url=="/create-user" && method=="POST"){
         const name = []
         req.on('data',(username)=>{

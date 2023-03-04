3629686f819519c83a3f0e07826d20d183d1b63c
r4ghuveer <raghuveerofficial08@gmail.com>
2023-03-04 23:17:12 +0530

    -

--- Backend_Node_js/app.js
+++ Backend_Node_js/app.js
@@ -13,14 +13,14 @@ const server = http.createServer((req, res) => {
   }
   if (url === '/message' && method === 'POST') {
     const body = [];
-    req.on('data', (chunk) => {
+    req.on('data', (chunk) => { // node js stream the data in chunk by chunk form, here on is a event listener for any incoming data
       console.log(chunk);
       body.push(chunk);
     });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
+    req.on('end', () => {// when the chunk is received their is a buffer in which this on listener activates
+      const parsedBody = Buffer.concat(body).toString(); // That buffer is used to concatinate the data received in chunk format
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFileSync('message.txt', message); // view message.txt created 
     });
     res.statusCode = 302;
     res.setHeader('Location', '/');

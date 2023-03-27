f2ce703d513db090687a1a920a7829165dd98863
r4ghuveer <raghuveerofficial08@gmail.com>
2023-03-27 21:43:57 +0530

    Turn on button

--- Electron/main.js
+++ Electron/main.js
@@ -1,23 +1,39 @@
-const {app, BrowserWindow, Menu} = require("electron");
+const {app, BrowserWindow, Menu, ipcMain} = require("electron");
+const { spawn } = require('child_process'); // for using .exe file, and displaying the output text that comes in cmd.
+
 
 let win = null; 
 
 app.whenReady().then(()=> {
-    const emptyMenuTemplate = []
+    // const emptyMenuTemplate = []
   
-  // Build an empty menu from the template
-  const emptyMenu = Menu.buildFromTemplate(emptyMenuTemplate)
+    // // Build an empty menu from the template
+    // const emptyMenu = Menu.buildFromTemplate(emptyMenuTemplate)
   
-  // Set the application menu to the empty menu
-  Menu.setApplicationMenu(emptyMenu)
+    // // Set the application menu to the empty menu
+    // Menu.setApplicationMenu(emptyMenu)
     win = new BrowserWindow({
         width: 800,
         height: 600,
         resizable: false,
         webPreferences: {
             nodeIntegration: true, // Integrating node to electron 
+            contextIsolation: false,
         }
     })
-
     win.loadFile("index.html")
+    
+});
+ipcMain.on('start-tracy',()=>{
+    const exepath = 'G:\\Development_Training\\Electron\\exe_file\\Tracy.exe';
+    const child = spawn(exepath);
+    child.stdout.on('data',(data)=>{
+        console.log(`= > ${data}`);
+    }); 
+    child.stderr.on('data',(data)=>{
+        console.error(`=> ${data}`);
+    });
+    child.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
 });
---
+++ Electron/render.js
@@ -0,0 +1,7 @@
+const {ipcRenderer} = require('electron');
+
+const button1 = document.getElementById('btn');
+
+button1.addEventListener('click',()=>{
+    ipcRenderer.send('start-tracy');
+});

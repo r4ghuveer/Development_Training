493c460433842b4b9fe756fd949d84fd357546a8
r4ghuveer <raghuveerofficial08@gmail.com>
2023-04-02 03:57:50 +0530

    main.js

--- Electron/main.js
+++ Electron/main.js
@@ -4,13 +4,6 @@ const { spawn } = require('child_process'); // for using .exe file, and displayi
 let win = null; 
 
 app.whenReady().then(()=> {
-    // const emptyMenuTemplate = []
-  
-    // // Build an empty menu from the template
-    // const emptyMenu = Menu.buildFromTemplate(emptyMenuTemplate)
-  
-    // // Set the application menu to the empty menu
-    // Menu.setApplicationMenu(emptyMenu)
     win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -23,18 +16,17 @@ app.whenReady().then(()=> {
     win.loadFile("index.html")
     
 });
-ipcMain.on('start-tracy',()=>{
-    const exepath = 'G:\\Development_Training\\Electron\\exe_file\\Tracy.exe';
-    const child = spawn(exepath,['<your voice input>']);
-    child.stdout.on('data',(data)=>{
-        win.webContents.send('output',data);
-    }); 
 
-    
-    // child.stderr.on('data',(data)=>{
-    //     console.error(`=> ${data}`);
-    // });
-    // child.on('close', (code) => {
-    //     console.log(`child process exited with code ${code}`);
-    // });
+ipcMain.on('start-tracy',(event)=>{
+    const exepath = 'G:\\Development_Training\\Electron\\exe_file\\Tracy.exe';
+    const child = spawn(exepath);
+    child.stdout.on('data',(data)=>{
+        win.webContents.send('output',data.toString());
+    });
+    child.stderr.on('data',(data)=>{
+        console.error(`=> ${data}`);
+    });
+    child.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
 });

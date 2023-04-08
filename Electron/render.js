22cf39b909ea7c4ac953c6a9146e40b811c9e274
r4ghuveer <raghuveerofficial08@gmail.com>
2023-04-09 00:08:42 +0530

    -

--- Electron/render.js
+++ Electron/render.js
@@ -1,6 +1,14 @@
 const {ipcRenderer} = require('electron');
 const button1 = document.getElementById('btn');
 
+
+button1.addEventListener('mouseover', () => {
+  button1.style.cursor = 'pointer';
+});
+
+button1.addEventListener('mouseout', () => {
+  button1.style.cursor = 'default';
+});
 button1.addEventListener('click',()=>{
     ipcRenderer.send('start-tracy');
 });

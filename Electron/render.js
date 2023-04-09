fd155083def4b4529232a99e97f97f5162c86bda
r4ghuveer <raghuveerofficial08@gmail.com>
2023-04-09 23:40:50 +0530

    -

--- Electron/render.js
+++ Electron/render.js
@@ -4,13 +4,13 @@ const button1 = document.getElementById('btn');
 
 button1.addEventListener('mouseover', () => {
   button1.style.cursor = 'pointer';
-  if(button1.style.backgroundColor==='white'){
     button1.style.backgroundColor='white';
-  }
+
 });
 
 button1.addEventListener('mouseout', () => {
   button1.style.cursor = 'default';
+  button1.style.backgroundColor='bisque';
 });
 button1.addEventListener('click',()=>{
     ipcRenderer.send('start-tracy');

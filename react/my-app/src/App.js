79bfb2f68d11e921a7cd2a543e7701fe11dfc2ed
r4ghuveer <raghuveerofficial08@gmail.com>
2023-04-16 17:11:33 +0530

    TextForm

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
     <Navbar title="First Nav" aboutText="This is the first prop test"/> 
+    <div className="container">
+      <TextForm heading="Enter the text"/>
+
+    </div>
     </>
   );
 }

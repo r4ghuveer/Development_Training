ad3b04f51096586ec16497edb9742f58a7e542c7
r4ghuveer <raghuveerofficial08@gmail.com>
2023-04-18 21:47:22 +0530

    textform.js

--- react/my-app/src/components/TextForm.js
+++ react/my-app/src/components/TextForm.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function TextForm(props) {
+    const [text,setText] = useState('Enter the text'); //here text is a variable, 'enter the text' is a default string for text, and setText is the function that we will call to give value to text variable.
   return (
     <div>
         <div class="mb-3">
         <label for="mybox" class="form-label">{props.heading}</label>
-        <textarea class="form-control" id="mybox" rows="3"></textarea>
+        <textarea class="form-control" id="mybox" rows="8"></textarea>
+        
         </div>
+        <button class="btn-upper">Uppercase</button>
     </div>
   )
 }

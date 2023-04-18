6bd90fef35848cb81bf0ff3db3a8748bbacf1b5b
r4ghuveer <raghuveerofficial08@gmail.com>
2023-04-18 21:58:42 +0530

    -

--- react/my-app/src/components/TextForm.js
+++ react/my-app/src/components/TextForm.js
@@ -6,8 +6,9 @@ export default function TextForm(props) {
       console.log("convert but was clicked");
       setText("You have click uppercase button");
     };
-    const handleOnChange=()=>{
+    const handleOnChange=(event)=>{
       console.log("on change");
+      setText(event.target.value);
     };
   return (
     <div>

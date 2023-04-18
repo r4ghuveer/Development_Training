de63579eb4a28c0bc375741d1dc3eddeb8d4d974
r4ghuveer <raghuveerofficial08@gmail.com>
2023-04-18 21:56:48 +0530

    -

--- react/my-app/src/components/TextForm.js
+++ react/my-app/src/components/TextForm.js
@@ -2,14 +2,21 @@ import React,{useState} from 'react'
 
 export default function TextForm(props) {
     const [text,setText] = useState('Enter the text'); //here text is a variable, 'enter the text' is a default string for text, and setText is the function that we will call to give value to text variable.
+    const HandleUpClick= () =>{
+      console.log("convert but was clicked");
+      setText("You have click uppercase button");
+    };
+    const handleOnChange=()=>{
+      console.log("on change");
+    };
   return (
     <div>
         <div class="mb-3">
         <label for="mybox" class="form-label">{props.heading}</label>
-        <textarea class="form-control" id="mybox" rows="8"></textarea>
+        <textarea class="form-control" id="mybox" rows="8" value={text} onChange={handleOnChange}></textarea>
         
         </div>
-        <button class="btn-upper">Uppercase</button>
+        <button class="btn-upper" onClick={HandleUpClick()}>Uppercase</button>
     </div>
   )
 }

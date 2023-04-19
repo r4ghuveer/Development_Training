92868054d89737e4ee9259715920f41af1ad730c
r4ghuveer <raghuveerofficial08@gmail.com>
2023-04-19 23:21:41 +0530

    -

--- react/my-app/src/components/TextForm.js
+++ react/my-app/src/components/TextForm.js
@@ -12,6 +12,7 @@ export default function TextForm(props) {
     };
      const [text,setText] = useState('Enter the text'); //here text is a variable, 'enter the text' is a default string for text, and setText is the function that we will call to give value to text variable.
   return (
+    
     <div>
         <div class="mb-3">
         <label for="mybox" class="form-label">{props.heading}</label>

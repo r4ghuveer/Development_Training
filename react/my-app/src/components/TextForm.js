8e76ddc4ac76c327f51e03b74ede04ecb97d8820
r4ghuveer <raghuveerofficial08@gmail.com>
2023-04-30 12:07:46 +0530

    =

--- react/my-app/src/components/TextForm.js
+++ react/my-app/src/components/TextForm.js
@@ -1,10 +1,10 @@
-import React,{useState} from 'react'
+import React,{useState} from 'react';
 
 export default function TextForm(props) {
     const HandleUpClick= () =>{
       console.log("convert but was clicked");
       let up = text.toUpperCase();
-      setText(up);
+      setText(up); //here setText is a hook (since we are not able to directly change state we need to use setText hook)
     };
     const handleOnChange=(event)=>{
       console.log("on change");
@@ -13,6 +13,7 @@ export default function TextForm(props) {
     
      const [text,setText] = useState('Enter the text'); //here text is a variable, 'enter the text' is a default string for text, and setText is the function that we will call to give value to text variable.
   return (
+    <>
     <div>
         <div class="mb-3">
         <label for="mybox" class="form-label">{props.heading}</label>
@@ -21,5 +22,9 @@ export default function TextForm(props) {
         </div>
         <button class="btn-upper" onClick={HandleUpClick}>Uppercase</button>
     </div>
+    <div className="container">
+      
+    </div>
+    </>
   )
 }

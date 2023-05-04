80ff13bb868c50c5ac8a49105beaa8525b99431e
r4ghuveer <raghuveerofficial08@gmail.com>
2023-05-04 23:38:20 +0530

    -

---
+++ Backend_Node_js/1st_Express/Express_router.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+
+
+app.listen(3000);
+

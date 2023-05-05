e88bc01c262bc233202f20ea53d7ba577835959e
r4ghuveer <raghuveerofficial08@gmail.com>
2023-05-05 21:15:48 +0530

    -

--- Backend_Node_js/1st_Express/routes/shop.js
+++ Backend_Node_js/1st_Express/routes/shop.js
@@ -1,4 +1,6 @@
+const express = require('express');
+const router=express.Router();
 
-app.use('/',(req,res,next)=>{
+router.use('/',(req,res,next)=>{
     res.send('<h1>Hello from Express</h1>');
 })

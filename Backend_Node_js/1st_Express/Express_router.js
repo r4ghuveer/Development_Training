9df35a0e77b4025eb2dedc2ce2ffa6edd4eb4ed3
r4ghuveer <raghuveerofficial08@gmail.com>
2023-05-13 23:11:55 +0530

    -

--- Backend_Node_js/1st_Express/Express_router.js
+++ Backend_Node_js/1st_Express/Express_router.js
@@ -7,6 +7,10 @@ const shopRoutes = require('./routes/shop');
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.get("/shops",(req,res,next)=>{
+    res.send("<p> this is the shop </p>");
+})
+
 app.use((req,res,next)=>{
     res.status(404).send("<h3> Page not found </h3>");
 });

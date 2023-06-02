8a9804526a40f32fa499bf1467c09bcb6e96df15
r4ghuveer <raghuveerofficial08@gmail.com>
2023-06-02 19:40:43 +0530

    express-assignment

---
+++ Backend_Node_js/Express_assignment/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const homeRoute = require('./routes/home.js');
+const userRoute = require('./routes/user.js');
+
+app.use(homeRoute);
+app.use(userRoute);
+
+app.listen(3000);

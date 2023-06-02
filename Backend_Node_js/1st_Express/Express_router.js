fc1790a665daf204c775e773c1a8f6509adf1316
r4ghuveer <raghuveerofficial08@gmail.com>
2023-06-02 05:43:47 +0530

    add,and,shop

--- Backend_Node_js/1st_Express/Express_router.js
+++ Backend_Node_js/1st_Express/Express_router.js
@@ -12,6 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/admin', adminRoutes);
 app.use('/shop', shopRoutes);
 
+app.use((req,res,next)=>{
+  res.status(404).send('<h2> Page not found ! </h2>');
+})
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
--- Backend_Node_js/1st_Express/routes/shop.js
+++ Backend_Node_js/1st_Express/routes/shop.js
@@ -3,5 +3,4 @@ const router=express.Router();
 router.get('/shoes',(req,res,next)=>{
     res.send('<h1>Buy some shoes !</h1>');
 })
-
 module.exports=router;

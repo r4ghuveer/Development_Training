ccf7188890418db19b277c1475725b39a2fdb4f9
r4ghuveer <raghuveerofficial08@gmail.com>
2023-06-08 16:01:47 +0530

    product_app

--- Backend_Node_js/Express_assignment/index.js
+++ Backend_Node_js/Express_assignment/index.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const homeRoute = require('./routes/home');
+const homeData = require('./routes/home');
 const userRoute = require('./routes/user');
 
-app.use(homeRoute);
+app.use(homeData.routes);
 app.use(userRoute);
 
 app.listen(3000);
--- Backend_Node_js/Express_assignment/routes/home.js
+++ Backend_Node_js/Express_assignment/routes/home.js
@@ -2,8 +2,17 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+const products=[];
+
 router.get('/home',(req,res,next)=>{
     res.sendFile(path.join(__dirname,'../','views','home.html'));
 })
 
-module.exports=router;
+router.post('/home',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/user');
+})
+
+// module.exports=router;
+exports.routes=router;
+exports.products=products;//multiple exports

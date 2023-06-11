7b256a2f23afde37ef2518d8989491454f7704ca
r4ghuveer <raghuveerofficial08@gmail.com>
2023-06-11 16:14:49 +0530

    model with json file data storage

--- Backend_Node_js/using_ejs/models/product.js
+++ Backend_Node_js/using_ejs/models/product.js
@@ -1,25 +1,15 @@
-const fs = require('fs');
-const mainModuleFilename = require.main.filename;
-const path = require ('path');
-module.exports= class Product {
-    constructor(t){
+const products = [];
+
+module.exports = class Product {
+    constructor(t) {
         this.title = t;
     }
+
     save() {
-        const p = path.join(path.dirname(mainModuleFilename),'data','products.json');
-        fs.readFile(p,(err,fileContent) =>{
-            let products=[];
-            if(!err){
-                products=JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p,JSON.stringify(products),(err)=>{
-                console.log(err);
-            });
-        });
+        products.push(this);
     }
-    static fetchAll () {
+
+    static fetchAll() {
         return products;
     }
-    
 }

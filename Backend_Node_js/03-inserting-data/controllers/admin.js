98ad9d63738953e87f3dfa2e6de13fc06ed558a2
r4ghuveer <raghuveerofficial08@gmail.com>
2023-07-04 00:13:00 +0530

    -

--- Backend_Node_js/03-inserting-data/controllers/admin.js
+++ Backend_Node_js/03-inserting-data/controllers/admin.js
@@ -1,3 +1,4 @@
+const { restart } = require('nodemon');
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -13,13 +14,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch(err => console.log(err));
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  })
+  .then((result) =>{ 
+    // console.log(result);
+    console.log("Created product");
+  })
+  .catch(err => console.log(err))
 };
 
 exports.getEditProduct = (req, res, next) => {

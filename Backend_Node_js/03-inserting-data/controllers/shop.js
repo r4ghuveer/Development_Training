594cf9c616758bcacceea53d9482d3412ad7eb3a
r4ghuveer <raghuveerofficial08@gmail.com>
2023-07-04 22:33:46 +0530

    details

--- Backend_Node_js/03-inserting-data/controllers/shop.js
+++ Backend_Node_js/03-inserting-data/controllers/shop.js
@@ -2,36 +2,35 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render('shop/product-list', {
-        prods: rows,
-        pageTitle: 'All Products',
-        path: '/products'
-      });
-    })
-    .catch(err => console.log(err));
+  Product
+  .findAll()// its basically select *
+  .then(products=>{
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  })
+  .catch(err => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-  .then(([product])=>{
+  // Product.findAll({where: {id: prodId}}).then().catch();
+  Product.findByPk(prodId)
+  .then(product=>{
     res.render('shop/product-detail', {
-      product: product[0],
+      product: product,
       pageTitle: product.title,
       path: '/products'
     });
   })
   .catch(err => console.log(err));
-  prodId, product => {
-    
-  }
 };
 
 exports.getIndex = (req, res, next) => {
   Product
-  .findAll()// its basically select * from
+  .findAll()// its basically select *
   .then(products=>{
     res.render('shop/index', {
       prods: products,

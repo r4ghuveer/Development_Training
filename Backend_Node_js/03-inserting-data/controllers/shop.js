ed28b7e8728c7b976b98680fdff41666ee400e1c
r4ghuveer <raghuveerofficial08@gmail.com>
2023-07-07 22:01:59 +0530

    -

--- Backend_Node_js/03-inserting-data/controllers/shop.js
+++ Backend_Node_js/03-inserting-data/controllers/shop.js
@@ -44,27 +44,17 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user.getCart()
   .then(cart =>{
-    console.log(cart);
+    return cart.getProduct()
+    .then(products=>{
+      res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+          });
+    })
+    .catch(err=>console.log(err));
   })
-  .catch(err=>console.log(err))
-  // Cart.getCart(cart => {
-  //   Product.fetchAll(products => {
-  //     const cartProducts = [];
-  //     for (product of products) {
-  //       const cartProductData = cart.products.find(
-  //         prod => prod.id === product.id
-  //       );
-  //       if (cartProductData) {
-  //         cartProducts.push({ productData: product, qty: cartProductData.qty });
-  //       }
-  //     }
-  //     res.render('shop/cart', {
-  //       path: '/cart',
-  //       pageTitle: 'Your Cart',
-  //       products: cartProducts
-  //     });
-  //   });
-  // });
+  .catch(err=>console.log(err));
 };
 
 exports.postCart = (req, res, next) => {

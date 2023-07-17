bda87143e267a3bb810b34c97d12861dfb5d79d2
r4ghuveer <raghuveerofficial08@gmail.com>
2023-07-17 16:05:02 +0530

    -

--- Backend_Node_js/03-inserting-data/models/product.js
+++ Backend_Node_js/03-inserting-data/models/product.js
@@ -1,38 +1,15 @@
-// const db = require('../util/database');
+const mongoConnect = require('../util/database')
+class Product{
+  constructor(title,price ,description ,imageUrl){
+      this.price=price;
+      this.imageUrl=imageUrl;
+      this.title=title;
+      this.description=description;
+  }
+  save(){
 
-// const Cart = require('./cart');
-
-// module.exports = class Product {
-//   constructor(id, title, imageUrl, description, price) {
-//     this.id = id;
-//     this.title = title;
-//     this.imageUrl = imageUrl;
-//     this.description = description;
-//     this.price = price;
-//   }
-
-//   save() {
-//     return db.execute(
-//       'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-//       [this.title, this.price, this.imageUrl, this.description]
-//     );
-//   }
-
-//   static deleteById(id) {}
-
-//   static fetchAll() {
-//     return db.execute('SELECT * FROM products');
-//   }
-
-//   static findById(id) {
-//     return db.execute('SELECT * FROM products WHERE products.id = ?',[id]);
-//   }
-// };
-
-
-const Sequelize = require('sequelize');
-
-const sequelize = require('../util/database');
+  }
+}
 
 const Product = sequelize.define('product',{
   id: {
